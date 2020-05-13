11b0a9bb25d73670c00a5aa61eed20034a595b0e
edward <edward.yu0830@gmail.com>
2020-05-13 13:53:19 +0800

    final version

--- FirstApp/requestHandlers.js
+++ FirstApp/requestHandlers.js
@@ -1,6 +1,8 @@
 var querystring = require("querystring");
 var fs = require("fs");
-function start(response,postData){
+var formidable = require("formidable");
+
+function start(response){
 	console.log("Request handler 'start' was called.");
 
 	var body = '<html>'+
@@ -9,9 +11,9 @@ function start(response,postData){
 		'charset = UTF-8" />'+
 		'</head>'+
 		'<body>'+
-		'<form action="/upload" method="post">'+
-		'<textarea name="text" rows="20" cols="60"></textarea>'+
-		'<input type="submit" value="Submit text" />'+
+		'<form action="/upload" enctype="multipart/form-data" method="post">'+
+		'<input type="file" name="upload">'+
+		'<input type="submit" value="Upload file" />'+
 		'</form>'+
 		'</body>'+
 		'</html>';
@@ -20,14 +22,22 @@ function start(response,postData){
 	response.write(body);
 	response.end();
 }
-function upload(response,postData){
+function upload(response,request){
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type":"text/plain"});
-	response.write("You've sent: "+querystring.parse(postData).text);
-	response.end();
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request,function(error,fields,files){
+		console.log("parsing done");
+		fs.renameSync(files.upload.path,"C:\\Users\\User\\AppData\\Local\\Temp\\test.png");
+		response.writeHead(200, {"Content-Type":"text/html"});
+		response.write("recieved image:<br>");
+		response.write("<img src='/show' />");
+		response.end();
+	})
 }
 
-function show(response,postData){
+function show(response){
 	console.log("Request handler 'show' was called.");
 	fs.readFile("C:\\Users\\User\\AppData\\Local\\Temp\\test.png","binary",function(error,file){
 		if(error){
--- FirstApp/server.js
+++ FirstApp/server.js
@@ -2,23 +2,11 @@ var http = require("http");
 var url = require("url");
 
 function start(route,handle) {
-  function onRequest(request, response) {
-  	var postData = "";
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-
-    request.setEncoding("utf8");
-
-    request.addListener("data",function(postDataChunk){
-    	postData += postDataChunk;
-    	console.log("received POST data chunk '"+
-    				postDataChunk + "'.");
-    });
-
-    request.addListener("end",function(){
-    	route(handle,pathname,response,postData);
-    })
-  }
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		route(handle,pathname,response,request);
+	}
 
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
